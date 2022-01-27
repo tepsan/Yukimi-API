9be4a83748569406b393037fda81b8dd070a0d35
FaisalAli <faisalali1901@gmail.com>
2022-01-27 11:41:37 +0530

    added helmet for security

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+const helmet = require('helmet');
+
 const data = require('./data');
 
+app.use(cors());
+app.use(helmet());
+
 app.get('/', (req, res) => {
-  res.json({name: 'Hello World!'});
-})
+  res.json({ name: 'Hello World!' });
+});
 
 app.get('/users', (req, res) => {
   res.json({ data });
