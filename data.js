4134f29995a6368caae523c9349f33ab68b04023
FaisalAli <faisalali1901@gmail.com>
2022-02-21 12:36:13 +0530

    updated my address

--- data.js
+++ data.js
@@ -1,6 +1,7 @@
 const data = [
   '0xE170a089AF9B6Eb6fa00e3adCF097967462325Da',
   '0x748f8dC4D78dda3d83DF98e20FD185e116023b68',
+  '0x349aE19523245c36825c71C8D0e07166B9ca1eaE'
 ];
 
 module.exports = {
